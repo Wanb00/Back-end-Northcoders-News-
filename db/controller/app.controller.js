95bbd65d8b0045e3b4bbde79748e2b06b4dc4b40
Wanb00 <wansenglok6@gmail.com>
2025-04-28 16:12:01 +0100

    4-get-articles completed with tests and information added to endpoints.json

--- db/controller/app.controller.js
+++ db/controller/app.controller.js
@@ -1,22 +1,28 @@
 const endpoints = require('../../endpoints.json');
-const { selectTopics, selectArticleById,  } = require('../models/app.model')
+const { selectTopics, selectArticleById, selectArticles } = require('../models/app.model');
 
 const getEndpoints = (req, res, next) => {
     res.status(200).send({ endpoints });
-}
+};
 
 const getTopics = (req, res, next) => {
     return selectTopics()
         .then((topics) => {
-            res.status(200).send({ topics })
-        })
-}
+            res.status(200).send({ topics });
+        });
+};
 
 const getArticleById = (req, res, next) => {
     const { article_id } = req.params;
     return selectArticleById(article_id).then((article) => {
-        res.status(200).send({ article })
-    }).catch((err) => next(err))
-}
+        res.status(200).send({ article });
+    }).catch((err) => next(err));
+};
 
-module.exports = { getEndpoints, getTopics, getArticleById }
+const getArticles = (req, res, next) => {
+    return selectArticles().then((articles) => {
+        res.status(200).send({ articles });
+    });
+};
+
+module.exports = { getEndpoints, getTopics, getArticleById, getArticles };
