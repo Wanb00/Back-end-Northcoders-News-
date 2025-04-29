4ec222c4bb51644720b642b97c23f21093eaba46
Wanb00 <wansenglok6@gmail.com>
2025-04-29 14:22:41 +0100

    5-get-comments-by-articles

--- db/models/app.model.js
+++ db/models/app.model.js
@@ -44,4 +44,18 @@ const selectArticles = () => {
             });
 };
 
-module.exports = { selectTopics, selectArticleById, selectArticles};
+const selectCommentsByArticle = (article_id) => {
+    return db
+        .query(`SELECT * FROM comments WHERE article_id = $1 ORDER BY comments.created_at DESC;`, [article_id])
+        .then((result) => {
+            if (!result.rows[0]) {
+                return Promise.reject({
+                    status: 404,
+                    msg: 'Article Not Found'
+                });
+            } 
+            return result.rows;
+        })
+}
+
+module.exports = { selectTopics, selectArticleById, selectArticles, selectCommentsByArticle, };
