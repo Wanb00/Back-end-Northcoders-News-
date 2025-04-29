ebad33d8d87a7d47841898b4104372af602b1fa5
Wanb00 <wansenglok6@gmail.com>
2025-04-29 16:55:58 +0100

    7 patch article

--- db/models/app.model.js
+++ db/models/app.model.js
@@ -79,4 +79,18 @@ const insertCommentByArticle = (article_id, username, body) => {
         })
 }
 
-module.exports = { selectTopics, selectArticleById, selectArticles, selectCommentsByArticle, insertCommentByArticle };
+const selectOwnerByIdToUpdate = (article_id, inc_votes) => {
+    if (typeof inc_votes !== 'number') {
+      return Promise.reject({ status: 400, msg: 'Bad Request inc_votes must be a number' });
+    }
+      return db
+          .query(`UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *;`, [inc_votes, article_id])
+          .then((result) => {
+              if (result.rows.length === 0) {
+                  return Promise.reject({ status: 404, msg: 'Article Not Found' });
+                }
+              return result.rows[0];
+          })
+  }
+
+module.exports = { selectTopics, selectArticleById, selectArticles, selectCommentsByArticle, insertCommentByArticle, selectOwnerByIdToUpdate };
