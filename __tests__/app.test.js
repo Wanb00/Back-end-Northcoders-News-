909120e2edcf96f51f4bd18c4cabe3b5e241c9f3
Wanb00 <wansenglok6@gmail.com>
2025-05-01 16:45:38 +0100

    12-comment_count

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -66,7 +66,8 @@ describe('GET /api/articles/:article_id', () => {
         topic: 'mitch',
         created_at: '2020-11-03T09:12:00.000Z',
         votes: 0,
-        article_img_url: "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
+        article_img_url: "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        comment_count: expect.any(Number)
       })
     })
   })
