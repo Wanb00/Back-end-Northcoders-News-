9eb606c21224a7f30938e51d99c6d6946fbc8db9
Wanb00 <wansenglok6@gmail.com>
2025-05-20 15:31:04 +0100

    fixed connection console.log

--- db/connection.js
+++ db/connection.js
@@ -6,6 +6,12 @@ const config = {};
 
 require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
 
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+    throw new Error("No PGDATABASE or DATABSE_URL configured")
+} else { 
+    console.log(`Connected to ${process.env.DATABASE_URL}`)
+}
+
 if (ENV === 'production') {
     config.connectionString = process.env.DATABASE_URL;
     config.max = 2;
@@ -13,11 +19,6 @@ if (ENV === 'production') {
 
 const db = new Pool(config);
 
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-    throw new Error("No PGDATABASE or DATABSE_URL configured")
-} else { 
-    console.log(`Connected to ${process.env.PGDATABASE}`)
-}
 
 
 module.exports = db;
